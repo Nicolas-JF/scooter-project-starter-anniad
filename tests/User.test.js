9f0bacf9fb99a7bc0ec124de007b25dafb905ab1
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-04 10:43:59 -0500

    Updated User.js and User.test.js

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,20 +1,33 @@
-const User = require('../src/User');
-
+const User = require("../src/User");
 const user = new User("Joe Bloggs", "test123", 21);
-
 // User tests here
 describe("User property tests", () => {
   // test username
   test("username should be a string", () => {
     expect(typeof user.username).toBe("string");
-  })
+  });
   // test password
-  // Hello
-
+  test("password should be a string", () => {
+    expect(typeof user.password).toBe("string");
+  });
   // test age
-})
-
-
+  test("age should be a number", () => {
+    expect(typeof user.age).toBe("number");
+  });
+});
 // test login
+describe("User method tests", () => {
+  test("login with correct password", () => {
+    user.login("test123");
+    expect(user.loggedIn).toBe(true);
+  });
+  // test logout
+  test("logout should work", () => {
+    user.logout();
+    expect(user.loggedIn).toBe(false);
+  });
+});
 
-// test logout
+
+
+
